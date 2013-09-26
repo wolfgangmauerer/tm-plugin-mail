e9bb1720a55c5c565efbff35fe5fc54d2dc6510b
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-26 10:39:10 +0000

    Simplify
    git-svn-id: svn://r-forge.r-project.org/svnroot/tm-plugin-mail@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/source.R
+++ pkg/R/source.R
@@ -7,19 +7,17 @@ function(mbox, encoding = "unknown")
     offsets <- integer(0)
     lines <- integer(0)
     while (length(line <- readLines(f, 1)) == 1) {
-        newMessage <- length(grep("^From ", line, useBytes = TRUE))
-        if (newMessage) {
+        if (grepl("^From ", line, useBytes = TRUE)) {
             message.nr <- message.nr + 1
             offsets[message.nr] <- seek(f)
             lines[message.nr] <- 0
-        } else {
+        } else
             lines[message.nr] <- lines[message.nr] + 1
-        }
     }
     s <- tm::Source(encoding = encoding, length = length(lines),
                     vectorized = TRUE, class = "MBoxSource")
     s$File <- f
-    s$mbox <- mbox
+    s$Mbox <- mbox
     s$MessageOffsets <- offsets
     s$MessageLines <- lines
     s
@@ -31,7 +29,7 @@ function(x)
     seek(x$File, x$MessageOffsets[x$Position])
     list(content = readLines(x$File, x$MessageLines[x$Position],
                              encoding = x$Encoding),
-         uri = x$mbox)
+         uri = x$Mbox)
 }
 
 pGetElem.MBoxSource <-
@@ -42,7 +40,7 @@ function(x)
             seek(x$File, x$MessageOffsets[y])
             list(content = readLines(x$File, x$MessageLines[y],
                                      encoding = x$Encoding),
-                 uri = x$mbox)
+                 uri = x$Mbox)
         })
 }
 
