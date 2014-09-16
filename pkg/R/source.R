f846dc85a4c5b9b156cd63fdd6e77b80b5c87b7a
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-16 16:28:44 +0000

    Implement open() and close() for MBoxSource
    git-svn-id: svn://r-forge.r-project.org/svnroot/tm-plugin-mail@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/source.R
+++ pkg/R/source.R
@@ -1,33 +1,51 @@
 MBoxSource <-
 function(mbox, encoding = "")
 {
-    f <- file(mbox)
-    open(f)
-    on.exit(close(f))
-    message.nr <- 0
-    offsets <- integer(0)
-    lines <- integer(0)
-    while (length(line <- readLines(f, 1)) == 1) {
-        if (grepl("^From ", line, useBytes = TRUE)) {
-            message.nr <- message.nr + 1
-            offsets[message.nr] <- seek(f)
-            lines[message.nr] <- 0
-        } else
-            lines[message.nr] <- lines[message.nr] + 1
+    SimpleSource(encoding = encoding, file = NULL, length = 0,
+                 reader = readMail, mbox = mbox, msgLines = 0,
+                 msgOffsets = 0, class = "MBoxSource")
+}
+
+close.MBoxSource <-
+function(x)
+{
+    if (!is.null(x$file)) {
+        close(x$file)
+        x$file <- NULL
+        x$length <- x$msgLines <- x$msgOffsets <- 0
     }
-    SimpleSource(encoding = encoding, length = length(lines), reader = readMail,
-                 mbox = mbox, msgOffsets = offsets, msgLines = lines,
-                 class = "MBoxSource")
+    x
 }
 
 getElem.MBoxSource <-
 function(x)
 {
-    f <- file(x$mbox)
-    open(f)
-    on.exit(close(f))
-    seek(f, x$msgOffsets[x$position])
-    list(content = iconv(readLines(f, x$msgLines[x$position]),
+    stopifnot(!is.null(x$file))
+
+    seek(x$file, x$msgOffsets[x$position])
+    list(content = iconv(readLines(x$file, x$msgLines[x$position]),
                          x$encoding, "UTF-8", "byte"),
          uri = x$mbox)
+}
+
+open.MBoxSource <-
+function(x)
+{
+    x$file <- file(x$mbox)
+    open(x$file)
+    message.nr <- 0
+    offsets <- integer(0)
+    lines <- integer(0)
+    while (length(line <- readLines(x$file, 1)) == 1) {
+        if (grepl("^From ", line, useBytes = TRUE)) {
+            message.nr <- message.nr + 1
+            offsets[message.nr] <- seek(x$file)
+            lines[message.nr] <- 0
+        } else
+            lines[message.nr] <- lines[message.nr] + 1
+    }
+    x$length <- length(lines)
+    x$msgLines <- lines
+    x$msgOffsets <- offsets
+    x
 }
